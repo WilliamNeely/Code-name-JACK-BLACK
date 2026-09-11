0e68e39f9bfdf44972737c20fda7f09796413826
WilliamNeely <william.neely@malad.us>
2026-09-11 17:46:44 -0600

    Add files via upload

---
+++ Hello world Script.java
@@ -0,0 +1,11 @@
+class QuickStart {
+ public static final String ANSI_BLUE = "\u001B[34m";
+ public static final String ANSI_RESET = "\u001B[0m";
+ public static void main (String[] args) {
+   System.out.println(ANSI_BLUE + "Hello world" + ANSI_RESET);
+   System.out.println(ANSI_BLUE + "My Name Is William Im 14 Years Old" + ANSI_RESET);
+   System.out.println(ANSI_BLUE + " I have no siblings" + ANSI_RESET);
+   System.out.println(ANSI_BLUE + "  I have a Mom and a Dad" + ANSI_RESET);
+   System.out.println(ANSI_BLUE + "   I have a dog and 2 cats" + ANSI_RESET);
+ }  
+}
