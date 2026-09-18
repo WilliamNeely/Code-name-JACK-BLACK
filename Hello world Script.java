a7a0d0ee596c9b04b087205a82da351a7ce3fd0e
WilliamNeely <william.neely@malad.us>
2026-09-18 15:22:57 -0600

    Refactor Hello World script to include variables

--- Hello world Script.java
+++ Hello world Script.java
@@ -1,11 +1,15 @@
 class QuickStart {
- public static final String ANSI_BLUE = "\u001B[34m";
- public static final String ANSI_RESET = "\u001B[0m";
- public static void main (String[] args) {
-   System.out.println(ANSI_BLUE + "Hello world" + ANSI_RESET);
-   System.out.println(ANSI_BLUE + "My Name Is William Im 14 Years Old" + ANSI_RESET);
-   System.out.println(ANSI_BLUE + " I have no siblings" + ANSI_RESET);
-   System.out.println(ANSI_BLUE + "  I have a Mom and a Dad" + ANSI_RESET);
-   System.out.println(ANSI_BLUE + "   I have a dog and 2 cats" + ANSI_RESET);
- }  
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+
+        String name = "William";
+        int age = 14;
+
+        System.out.println(ANSI_BLUE + "Hello World My name is " + name + ", and I am " + age + " years old" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "I have no siblings" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "I have a Mom and a Dad" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "I have a dog and 2 cats" + ANSI_RESET);
+    }
 }
